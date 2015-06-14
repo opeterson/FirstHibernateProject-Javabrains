8fe0d58b9ddb3487a1f911b68f74f6cb478585c8
opeterson <owen.a.peterson@gmail.com>
2015-06-14 16:31:51 -0500

    Tutorial 3 part 3. Successfuly saved object to postgresql database.

---
+++ src/ca/owenpeterson/HibernateTest.java
@@ -0,0 +1,32 @@
+package ca.owenpeterson;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import ca.owenpeterson.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		UserDetails user = new UserDetails();
+		
+		user.setUserId(1);
+		user.setUserName("First User");
+		
+		//required for Hibernate 4 (differs from tutorial);
+		//tutorial mentions that a try catch should be used and do a rollback in the catch.
+		//will likely be covered later, as this is a starter.
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+		SessionFactory factory = configuration.buildSessionFactory((builder.build()));
+		
+		Session session = factory.openSession();
+		
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+	}
+
+}
